8d878d60175f9c780c7a20adf8bac27b7cd60f08
Smith-Chris1 <christopher.erick.smith@gmail.com>
2016-11-24 14:51:48 -0700

    updated function names

--- getActor.js
+++ getActor.js
@@ -1,8 +1,9 @@
-function getStar() {
-    var jsonHTTP = new XMLHttpRequest();
+function getStarID() {
+  var jsonHTTP = new XMLHttpRequest();
 	var api = 'api_key=8bd29dde4b31287cd5579e4bd90c80b3';
 	var url1 = 'https://api.themoviedb.org/3/search/person?';
 	var url2 = '&query=';
+	var callback = '&callback=person'
 	var name = encodeURIComponent(document.getElementById("starName").value);
 	var url = url1 + api + url2 + name;
 
@@ -11,12 +12,29 @@ jsonHTTP.open("GET", url, true);
 jsonHTTP.onreadystatechange=function() {
    if (jsonHTTP.readyState==4 && jsonHTTP.status==200) {
    		var data = JSON.parse(jsonHTTP.responseText);
-   		document.getElementById("json").innerHTML = "json = " + (jsonHTTP.responseText);
-   		document.getElementById("actorName").innerHTML = "data variable = " + data;
-    	document.getElementById("id").innerHTML = "id should be = " + (data[0].id); 
-   }
+      var id = data.results[0].id;
+      var bday = getStarBday(id);
+   		document.getElementById("json").innerHTML = "json = " + bday;
+}
+}
+jsonHTTP.send();
 }
 
-jsonHTTP.send();
+function getStarBday(id) {
+  var jsonHTTP = new XMLHttpRequest();
+	var api = '?api_key=8bd29dde4b31287cd5579e4bd90c80b3';
+	var url1 = 'https://api.themoviedb.org/3/person/';
+	var url2 = '&language=en-US';
+	var url = url1 + id + api + url2
 
-} 
+jsonHTTP.open("GET", url, true);
+
+jsonHTTP.onreadystatechange=function() {
+   if (jsonHTTP.readyState==4 && jsonHTTP.status==200) {
+   		var data = JSON.parse(jsonHTTP.responseText);
+      var id = data.results[0].id;
+   		document.getElementById("json").innerHTML = "json = " + url;
+}
+}
+jsonHTTP.send();
+}
