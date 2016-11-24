d05907b922ef3e0920435d2025d2f3d05c76197d
Smith-Chris1 <christopher.erick.smith@gmail.com>
2016-11-23 21:17:40 -0700

    Update getActor.js

--- getActor.js
+++ getActor.js
@@ -1,7 +1,7 @@
 function getStar() {
 	//var xmlhttp = new XMLHttpRequest();
 	var api = 'api_key=8bd29dde4b31287cd5579e4bd90c80b3'
-	var url1 = 'https://api.themoviedb.org/3/search/person?/'
+	var url1 = 'https://api.themoviedb.org/3/search/person?'
 	var url2 = '&querry='
 	var name = encodeURIComponent(document.getElementById("starName").value);
 	var url = url1 + api + url2 + name
