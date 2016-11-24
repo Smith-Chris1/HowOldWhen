38a33603a81f960a4eef46b94d46f01b3709e16c
Smith-Chris1 <christopher.erick.smith@gmail.com>
2016-11-23 22:42:26 -0700

    missed variable

--- getActor.js
+++ getActor.js
@@ -10,7 +10,7 @@ jsonHTTP.open("GET", url, true);
 
 jsonHTTP.onreadystatechange=function() {
    if (jsonHTTP.readyState==4 && jsonHTTP.status==200) {
-   		var data = JSON.parse(req.responseText);
+   		var data = JSON.parse(jsonHTTP.responseText);
       document.getElementById("actorName").innerHTML =data[0].id; 
    }
 }
