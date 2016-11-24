c99487dc3a1f41c8b716da1ac31da793b650e8d9
Smith-Chris1 <christopher.erick.smith@gmail.com>
2016-11-23 22:36:05 -0700

    json test

--- getActor.js
+++ getActor.js
@@ -1,26 +1,35 @@
 function getStar() {
-    var xmlhttp = new XMLHttpRequest();
+    var jsonHTTP = new XMLHttpRequest();
 	var api = 'api_key=8bd29dde4b31287cd5579e4bd90c80b3'
 	var url1 = 'https://api.themoviedb.org/3/search/person?'
 	var url2 = '&query='
 	var name = encodeURIComponent(document.getElementById("starName").value);
 	var url = url1 + api + url2 + name
 
-xmlhttp.onreadystatechange=function() {
-    if (this.readyState == 4 && this.status == 200) {
-        //myFunction(this.responseText);
-        var json = this.responseText;
-        var id = JSON.parse(json);
-        document.getElementById("actorName").innerHTML = json.page.id;
-    }
+jsonHTTP.open("GET", url, true);
+
+req.onreadystatechange=function() {
+   if (req.readyState==4 && req.status==200) {
+      document.getElementById("actorName").innerHTML = jsonHTTP.responseText; 
+   }
 }
 
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
-}
-function myFunction(response) {
-    var arr = JSON.parse(response);
-    var out = arr.id;
+req.send();
+//xmlhttp.onreadystatechange=function() {
+ //   if (this.readyState == 4 && this.status == 200) {
+ //       //myFunction(this.responseText);
+ //       var json = this.responseText;
+ //       var id = JSON.parse(json);
+ //       document.getElementById("actorName").innerHTML = json.page.id;
+ //   }
+//}
+
+//xmlhttp.open("GET", url, true);
+//xmlhttp.send();
+//}
+//function myFunction(response) {
+//    var arr = JSON.parse(response);
+//    var out = arr.id;
 
 
 //document.getElementById("actorName").innerHTML = arr;
