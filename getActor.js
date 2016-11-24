514c77f658ec6973426559984bfbca16dd1642ca
Smith-Chris1 <christopher.erick.smith@gmail.com>
2016-11-23 23:26:45 -0700

    jsonparse

--- getActor.js
+++ getActor.js
@@ -11,7 +11,7 @@ jsonHTTP.open("GET", url, true);
 
 jsonHTTP.onreadystatechange=function() {
    if (jsonHTTP.readyState==4 && jsonHTTP.status==200) {
-   		var data = jsonHTTP.responseText;
+   		var data = JSON.parse(jsonHTTP.responseText);
     	document.getElementById("actorName").innerHTML = (data[0].id); 
    }
 }
