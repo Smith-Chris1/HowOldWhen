2d027cddd1a94ad13d11a854e0e8ca65f7d95b86
Smith-Chris1 <christopher.erick.smith@gmail.com>
2016-11-23 22:41:26 -0700

    data variable added

--- getActor.js
+++ getActor.js
@@ -10,50 +10,9 @@ jsonHTTP.open("GET", url, true);
 
 jsonHTTP.onreadystatechange=function() {
    if (jsonHTTP.readyState==4 && jsonHTTP.status==200) {
-      document.getElementById("actorName").innerHTML = jsonHTTP.responseText; 
+   		var data = JSON.parse(req.responseText);
+      document.getElementById("actorName").innerHTML =data[0].id; 
    }
 }
 
 jsonHTTP.send();
-//xmlhttp.onreadystatechange=function() {
- //   if (this.readyState == 4 && this.status == 200) {
- //       //myFunction(this.responseText);
- //       var json = this.responseText;
- //       var id = JSON.parse(json);
- //       document.getElementById("actorName").innerHTML = json.page.id;
- //   }
-//}
-
-//xmlhttp.open("GET", url, true);
-//xmlhttp.send();
-//}
-//function myFunction(response) {
-//    var arr = JSON.parse(response);
-//    var out = arr.id;
-
-
-//document.getElementById("actorName").innerHTML = arr;
-}
-//function ajax(url, callback) {
-//  var xmlhttp = new XMLHttpRequest();
-//    xmlhttp.onreadystatechange = function(){
-//        if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
-//            callback(xmlhttp.responseText);
-//        } else {
-//            // handle the request
-//        }
-//    }
-//    xmlhttp.open("GET", url, true);
-//    xmlhttp.send();
-//}
-//
-//function requestData(endpoint) {
-//    var root = 'https://api.github.com';
-//    ajax(root + endpoint, function(data) {
-//        document.getElementById('output').innerHTML = data;
-//    });
-//}
-
-
-
-
