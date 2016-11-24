32ac44c8b7f7bd7518f0b99df28322b80c522c1a
Smith-Chris1 <christopher.erick.smith@gmail.com>
2016-11-23 21:41:20 -0700

    json call

--- getActor.js
+++ getActor.js
@@ -1,11 +1,37 @@
 function getStar() {
-	//var xmlhttp = new XMLHttpRequest();
+    var xmlhttp = new XMLHttpRequest();
 	var api = 'api_key=8bd29dde4b31287cd5579e4bd90c80b3'
 	var url1 = 'https://api.themoviedb.org/3/search/person?'
 	var url2 = '&query='
 	var name = encodeURIComponent(document.getElementById("starName").value);
 	var url = url1 + api + url2 + name
-	document.getElementById("actorName").innerHTML = url
+
+xmlhttp.onreadystatechange=function() {
+    if (this.readyState == 4 && this.status == 200) {
+        myFunction(this.responseText);
+    }
+}
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+
+function myFunction(response) {
+    var arr = JSON.parse(response);
+    var i;
+    var out = "<table>";
+    for(i = 0; i < arr.length; i++) {
+        out += "<tr><td>" +
+        arr[i].firstName +
+        "</td><td>" +
+        arr[i].lastName +
+        "</td><td>" +
+        arr[i].email +
+        "</td></tr>";
+    }
+    out += "</table>";
+    document.getElementById("actorName").innerHTML = out;
+
+
+
 };
 //function ajax(url, callback) {
 //  var xmlhttp = new XMLHttpRequest();
