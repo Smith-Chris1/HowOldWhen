5e82760b62225d7c99ac803769a39338b814507f
Smith-Chris1 <christopher.erick.smith@gmail.com>
2016-11-23 23:33:38 -0700

    another test that won't work

--- getActor.js
+++ getActor.js
@@ -1,11 +1,10 @@
 function getStar() {
     var jsonHTTP = new XMLHttpRequest();
-	var api = 'api_key=8bd29dde4b31287cd5579e4bd90c80b3'
-	var url1 = 'https://api.themoviedb.org/3/search/person?'
-	var url2 = '&query='
-	var callback = '&callback=test'
+	var api = 'api_key=8bd29dde4b31287cd5579e4bd90c80b3';
+	var url1 = 'https://api.themoviedb.org/3/search/person?';
+	var url2 = '&query=';
 	var name = encodeURIComponent(document.getElementById("starName").value);
-	var url = url1 + api + url2 + name
+	var url = url1 + api + url2 + name;
 
 jsonHTTP.open("GET", url, true);
 
