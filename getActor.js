c951fba9913d9254a6d4c61e09ca546801600c5b
Smith-Chris1 <christopher.erick.smith@gmail.com>
2016-11-23 21:44:12 -0700

    json

--- getActor.js
+++ getActor.js
@@ -17,17 +17,8 @@ xmlhttp.send();
 function myFunction(response) {
     var arr = JSON.parse(response);
     var i;
-    var out = "<table>";
-    for(i = 0; i < arr.length; i++) {
-        out += "<tr><td>" +
-        arr[i].firstName +
-        "</td><td>" +
-        arr[i].lastName +
-        "</td><td>" +
-        arr[i].email +
-        "</td></tr>";
-    }
-    out += "</table>";
+    var out = arr.id;
+    
     document.getElementById("actorName").innerHTML = out;
 
 
