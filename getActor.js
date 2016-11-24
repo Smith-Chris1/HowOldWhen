7994805b6ddf7db26cccc3743f8e899902ae09c4
Smith-Chris1 <christopher.erick.smith@gmail.com>
2016-11-23 23:44:31 -0700

    another test

--- getActor.js
+++ getActor.js
@@ -1,23 +1,26 @@
 function getStar() {
-    var jsonHTTP = new XMLHttpRequest();
-	var api = 'api_key=8bd29dde4b31287cd5579e4bd90c80b3';
+	var api = '&language=en-US&api_key=8bd29dde4b31287cd5579e4bd90c80b3';
 	var url1 = 'https://api.themoviedb.org/3/search/person?';
 	var url2 = '&query=';
 	var name = encodeURIComponent(document.getElementById("starName").value);
-	var url = url1 + api + url2 + name;
+	var url = url1 + url2 + name + api;
 
-jsonHTTP.open("GET", url, true);
+var data = "{}";
 
-jsonHTTP.onreadystatechange=function() {
-   if (jsonHTTP.readyState==4 && jsonHTTP.status==200) {
-   		var data = JSON.parse(jsonHTTP.responseText);
-   		document.getElementById("json").innerHTML = "json = " + (jsonHTTP.responseText);
-   		document.getElementById("actorName").innerHTML = "data variable = " + data;
-    	document.getElementById("id").innerHTML = "id should be = " + (data[0].id); 
-   }
-}
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
 
-jsonHTTP.send();
+xhr.addEventListener("readystatechange", function () {
+  if (this.readyState === this.DONE) {
+    document.getElementById("json").innerHTML = (this.responseText);
+  }
+});
+
+//xhr.open("GET", "https://api.themoviedb.org/3/search/person?query=bruce%20willis&language=en-US&api_key=8bd29dde4b31287cd5579e4bd90c80b3");
+
+xhr.open("GET", url);
+
+xhr.send(data);
 
 }
 
