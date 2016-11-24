58506fa8603f28ef24d6c1328da31c3df35b65d0
Smith-Chris1 <christopher.erick.smith@gmail.com>
2016-11-24 15:20:16 -0700

    bday variable

--- getActor.js
+++ getActor.js
@@ -32,8 +32,8 @@ jsonHTTP.open("GET", url, true);
 jsonHTTP.onreadystatechange=function() {
    if (jsonHTTP.readyState==4 && jsonHTTP.status==200) {
    		var data = JSON.parse(jsonHTTP.responseText);
-        //var id = data.results[0].id;
-   		document.getElementById("json").innerHTML = "json = " + jsonHTTP.responseText;
+        var bday = data[0].birthday;
+   		document.getElementById("json").innerHTML = "json = " + bday;
 }
 }
 jsonHTTP.send();
