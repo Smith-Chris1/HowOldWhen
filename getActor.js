0da0d7eda169c1e85d9e829a9293eb5b38b2adb9
Smith-Chris1 <christopher.erick.smith@gmail.com>
2016-11-24 17:26:51 -0700

    finished fixing javascript, html and added css

--- getActor.js
+++ getActor.js
@@ -1,3 +1,6 @@
+var id = ''
+var bday = ''
+var movie = ''
 function getStarID() {
   var jsonHTTP = new XMLHttpRequest();
 	var api = 'api_key=8bd29dde4b31287cd5579e4bd90c80b3';
@@ -13,8 +16,9 @@ jsonHTTP.onreadystatechange=function() {
    if (jsonHTTP.readyState==4 && jsonHTTP.status==200) {
    		var data = JSON.parse(jsonHTTP.responseText);
       var id = data.results[0].id;
-      var bday = getStarBday(id);
-   		//document.getElementById("json").innerHTML = "json = " + bday;
+      //display(id)
+      getStarBday(id);
+   	  //document.getElementById("json").innerHTML = "json = " + bday;
 }
 }
 jsonHTTP.send();
@@ -32,9 +36,44 @@ jsonHTTP.open("GET", url, true);
 jsonHTTP.onreadystatechange=function() {
    if (jsonHTTP.readyState==4 && jsonHTTP.status==200) {
    		var data = JSON.parse(jsonHTTP.responseText);
-        var bday = data[0].birthday;
-   		document.getElementById("json").innerHTML = "json = " + bday;
+        var bday = data.birthday;
+        getMovieList(id,bday)
+   		//document.getElementById("json").innerHTML = "json = " + bday;
 }
+}
+jsonHTTP.send();
+} 
+function display(title,releaseDate,bday) {
+    var table = document.getElementById('movie');
+    var tbody = table.getElementsByTagName('tbody');
+    var row = tbody[0].insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var date1 = releaseDate.substring(0, 4);
+    var date2 = bday.substring(0, 4);
+    var age = date1 - date2;
+    cell1.innerHTML = title;
+    cell2.innerHTML = age;
+}
+
+function getMovieList(id,bday) {
+  var jsonHTTP = new XMLHttpRequest();
+	var api = '/movie_credits?api_key=8bd29dde4b31287cd5579e4bd90c80b3&language=en-US';
+	var url1 = 'https://api.themoviedb.org/3/person/';
+	var url = url1 + id + api
+
+jsonHTTP.open("GET", url, true);
+
+jsonHTTP.onreadystatechange=function() {
+   if (jsonHTTP.readyState==4 && jsonHTTP.status==200) {
+   		var data = JSON.parse(jsonHTTP.responseText);
+        var movieArray = data;
+            for (var i = 0; i < movieArray.cast.length; i++) { 
+            var movie = (movieArray.cast[i].title);
+            var releaseDate = (movieArray.cast[i].release_date)
+            display(movie, releaseDate, bday)
+}
+  }
 }
 jsonHTTP.send();
 } 
