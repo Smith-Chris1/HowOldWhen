7bd64e58480bf7d8cab21611403bd96e4b275714
Smith-Chris1 <christopher.erick.smith@gmail.com>
2016-11-30 23:39:20 -0700

    updated the menus

--- getActor.js
+++ getActor.js
@@ -136,3 +136,10 @@ function display(title,releaseDate,bday,poster) {
     document.getElementById('results').appendChild(div).appendChild(p);
     }
 }
+function openNav() {
+    document.getElementById("myNav").style.width = "30%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
